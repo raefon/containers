c2d8514574a3ec2bf6ccec5b5624c85714b1cd8e
raefon <raefon@users.noreply.github.com>
2023-12-22 21:59:10 -0800

    Update sabnzbd

--- apps/sabnzbd/entrypoint.sh
+++ apps/sabnzbd/entrypoint.sh
@@ -4,21 +4,21 @@
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
 
-if [[ ! -f "/config/sabnzbd.ini" ]]; then
+if [[ ! -f "/config/sabnzbd/sabnzbd.ini" ]]; then
     printf "Copying over default configuration ...\n"
     mkdir -p /config/sabnzbd
-    cp /app/sabnzbd.ini /config/sabnzbd.ini
+    cp /app/sabnzbd.ini /config/sabnzbd/sabnzbd.ini
 
     printf "Creating api keys ...\n"
     api_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
     nzb_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-    sed -i -e "s/^api_key *=.*$/api_key = ${api_key}/g" /config/sabnzbd.ini
-    sed -i -e "s/^nzb_key *=.*$/nzb_key = ${nzb_key}/g" /config/sabnzbd.ini
+    sed -i -e "s/^api_key *=.*$/api_key = ${api_key}/g" /config/sabnzbd/sabnzbd.ini
+    sed -i -e "s/^nzb_key *=.*$/nzb_key = ${nzb_key}/g" /config/sabnzbd/sabnzbd.ini
 fi
 
-[[ -n "${SABNZBD__API_KEY}" ]] && sed -i -e "s/^api_key *=.*$/api_key = ${SABNZBD__API_KEY}/g" /config/sabnzbd.ini
-[[ -n "${SABNZBD__NZB_KEY}" ]] && sed -i -e "s/^nzb_key *=.*$/nzb_key = ${SABNZBD__NZB_KEY}/g" /config/sabnzbd.ini
-[[ -n "${SABNZBD__HOST_WHITELIST_ENTRIES}" ]] && sed -i -e "s/^host_whitelist *=.*$/host_whitelist = ${HOSTNAME:-sabnzbd}, ${SABNZBD__HOST_WHITELIST_ENTRIES}/g" /config/sabnzbd.ini
+[[ -n "${SABNZBD__API_KEY}" ]] && sed -i -e "s/^api_key *=.*$/api_key = ${SABNZBD__API_KEY}/g" /config/sabnzbd/sabnzbd.ini
+[[ -n "${SABNZBD__NZB_KEY}" ]] && sed -i -e "s/^nzb_key *=.*$/nzb_key = ${SABNZBD__NZB_KEY}/g" /config/sabnzbd/sabnzbd.ini
+[[ -n "${SABNZBD__HOST_WHITELIST_ENTRIES}" ]] && sed -i -e "s/^host_whitelist *=.*$/host_whitelist = ${HOSTNAME:-sabnzbd}, ${SABNZBD__HOST_WHITELIST_ENTRIES}/g" /config/sabnzbd/sabnzbd.ini
 
 #shellcheck disable=SC2086
 exec \
@@ -26,5 +26,5 @@ exec \
         /app/SABnzbd.py \
         --browser 0 \
         --server 0.0.0.0:${SABNZBD__PORT:-8080} \
-        --config-file /config/sabnzbd.ini \
+        --config-file /config/sabnzbd/sabnzbd.ini \
         "$@"
