9e370e876bc8f7640f9575c9b095a12b05c6a1e5
raefon <raefon@users.noreply.github.com>
2024-10-22 08:31:34 -0700

    Fix postgres-init latest.sh

--- apps/postgres-init/ci/latest.sh
+++ apps/postgres-init/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=postgresql15-client&branch=v3.18&arch" | grep -oP '(?<=<td class="version">)[^<]*' 2>/dev/null)
+version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=postgresql15-client&branch=v3.18&arch" | sed -n 's/.*<strong class="hint--right hint--rounded text-success" aria-label="Package up-to-date">\([^<]*\)<.*/\1/p' | sed 's/\\//g' | head -n 1)
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
