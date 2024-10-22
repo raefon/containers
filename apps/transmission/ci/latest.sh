ecbc1f83b10dbc5ef86dcb8e758c15b565e9ffef
raefon <raefon@users.noreply.github.com>
2024-10-21 20:19:09 -0700

    Update transmission latest.sh ci script

--- apps/transmission/ci/latest.sh
+++ apps/transmission/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-useragent="Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0"
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=transmission&inrepo=alpine_3_19" -A $useragent | jq -r '.transmission | .[] | select((.repo == "alpine_3_19" and .binname == "transmission-daemon")) | .origversion')
+user_agent="User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11"
+version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=transmission&inrepo=alpine_3_19" -H $user_agent | jq -r '.transmission | .[] | select((.repo == "alpine_3_19" and .binname == "transmission-daemon")) | .origversion')
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
