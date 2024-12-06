848805001aad1d6309f446ea7a756ffe6cc73680
raefon <raefon@users.noreply.github.com>
2024-12-06 11:45:10 -0800

    Fix: update latest.sh version repo

--- apps/jellyfin/ci/latest.sh
+++ apps/jellyfin/ci/latest.sh
@@ -2,7 +2,7 @@
 channel=$1
 
 if [[ "${channel}" == "stable" ]]; then
-    version=$(curl -sX GET https://repo.jellyfin.org/ubuntu/dists/noble/main/binary-amd64/Packages |grep -A 7 -m 1 'Package: jellyfin-server' | awk -F ': ' '/Version/{print $2;exit}');
+    version=$(curl -sX GET https://repo.jellyfin.org/ubuntu/dists/jammy/main/binary-amd64/Packages |grep -A 7 -m 1 'Package: jellyfin-server' | awk -F ': ' '/Version/{print $2;exit}');
 fi
 
 version="${version#*v}"
