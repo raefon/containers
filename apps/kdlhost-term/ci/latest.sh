5d271280299f434e9de894aa0895e70f1808ef6d
raefon <raefon@users.noreply.github.com>
2024-12-15 18:34:16 -0800

    Update: kdlhost-term

--- apps/kdlhost-term/ci/latest.sh
+++ apps/kdlhost-term/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version="1.0.1"
+version="1.0.2"
 printf "%s" "${version}"
--- apps/kdlhost-term/scripts/kdlhost-tmux.sh
+++ apps/kdlhost-term/scripts/kdlhost-tmux.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-tmux -f /app/kdlhost-term/restricted.tmux.conf new-session -A -s kdlhost-term 'supercronic -inotify /config/kdlhost-term/crontab' \; \
-    split-window -hv 'fish' \;
+tmux -f /app/kdlhost-term/restricted.tmux.conf new-session -A -s kdlhost-term 'fish' \; \
+    split-window -v -p 33 'supercronic -inotify /config/kdlhost-term/crontab' \; \
+    select-pane -t 0 \; \
+    attach-session \;
