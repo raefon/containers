84dee15331eefc7dcd4389c6b178503f310d4d1c
raefon <raefon@users.noreply.github.com>
2025-07-09 12:17:35 -0700

    Update ci for tautulli

--- apps/tautulli/ci/latest.sh
+++ apps/tautulli/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/Tautulli/Tautulli/releases/latest" | jq --raw-output '. | .tag_name' 2>/dev/null)
-version="${version#*v}"
+version=$(curl -sX GET https://api.github.com/repos/Tautulli/Tautulli/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 version="${version#*release-}"
 printf "%s" "${version}"
