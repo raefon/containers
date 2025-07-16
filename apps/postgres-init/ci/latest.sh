38a9bb3ea8bdfb828f35c8de9d996a3ee52a3826
raefon <raefon@users.noreply.github.com>
2025-07-15 20:53:28 -0700

    Depreciate various containers

--- apps/postgres-init/ci/latest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-user_agent="User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11"
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=postgresql&inrepo=alpine_3_19" -H $user_agent | jq -r '.postgresql | .[] | select((.repo == "alpine_3_19" and .binname == "postgresql15")) | .origversion')
-version="${version%%_*}"
-version="${version%%-*}"
-printf "%s" "${version}"
