904d9d522bdeac3199f46ca9481f227c03107cb6
raefon <raefon@users.noreply.github.com>
2025-07-15 20:26:55 -0700

    Update volsync

--- apps/volsync/ci/latest.sh
+++ apps/volsync/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/backube/volsync/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
-version="${version#*v}"
+version=$(curl -sX GET https://api.github.com/repos/backube/volsync/releases/latest --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | .tag_name')
 version="${version#*release-}"
 printf "%s" "${version}"
