4317f5cc661a408a5f5d157c0040b8b7a9de0660
raefon <raefon@users.noreply.github.com>
2026-05-29 12:56:18 -0700

    add seerr application with Dockerfile, entrypoint, and CI scripts

---
+++ apps/seerr/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#shellcheck disable=SC1091
+test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
+
+#shellcheck disable=SC2155
+export HOME="/config/seerr" \
+    CONFIG_DIRECTORY="/config/seerr"
+
+if [[ ! -f "/config/seerr" ]]; then
+    printf "Copying over default configuration ...\n"
+    mkdir -p /config/seerr
+fi
+
+# Change to the /app/seerr directory
+cd /app/seerr || exit 1
+
+#shellcheck disable=SC2086
+exec /usr/bin/pnpm start
