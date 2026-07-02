8234f08581fe5b9ad2a88fa259e64803756bea26
raefon <raefon@users.noreply.github.com>
2026-07-02 07:06:57 -0700

    Update filebrowser to use filebrowser quantam

--- apps/filebrowser/entrypoint.sh
+++ apps/filebrowser/entrypoint.sh
@@ -3,30 +3,31 @@
 #shellcheck disable=SC1091
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
-if [[ ! -f "/config/filebrowser/settings.json" ]]; then
+if [[ ! -f "/config/filebrowser/config.yaml" ]]; then
     printf "Copying over default configuration ...\n"
     mkdir -p /config/filebrowser
-    cp /app/filebrowser/settings.json /config/filebrowser/settings.json
+    cp /app/filebrowser/config.yaml /config/filebrowser/config.yaml
+    /filebrowser set -u bogus,bogus -a -c config.yaml
 fi
 
-if [[ ! -f "/config/filebrowser/filebrowser.db" ]]; then
-    printf "Generating default database ...\n"
-    /filebrowser config init --disable-preview-resize \
-        --disable-thumbnails \
-        --disable-type-detection-by-header \
-        --branding.name="filebrowser, by KDLHOST " \
-        --branding.files=/branding \
-        --branding.disableExternal \
-        --auth.method=noauth \
-        --lockPassword \
-        --database /config/filebrowser/filebrowser.db \
-        --cache-dir /tmp \
-        --port 80
-    # allow commands
-    /filebrowser config set --database /config/filebrowser/filebrowser.db --commands zip,unzip,rar,unrar,ls,pwd,cd,mv,cp,ln,find,echo,grep,cat,touch,tar,gzip,rm,tree,du,mlocate,updatedb,locate,tenet
-    # add bogus user
-    /filebrowser users add 1 bogus --database /config/filebrowser/filebrowser.db
-fi
+# if [[ ! -f "/config/filebrowser/filebrowser.db" ]]; then
+#     printf "Generating default database ...\n"
+#     /filebrowser config init --disable-preview-resize \
+#         --disable-thumbnails \
+#         --disable-type-detection-by-header \
+#         --branding.name="filebrowser, by KDLHOST " \
+#         --branding.files=/branding \
+#         --branding.disableExternal \
+#         --auth.method=noauth \
+#         --lockPassword \
+#         --database /config/filebrowser/filebrowser.db \
+#         --cache-dir /tmp \
+#         --port 80
+#     # allow commands
+#     /filebrowser config set --database /config/filebrowser/filebrowser.db --commands zip,unzip,rar,unrar,ls,pwd,cd,mv,cp,ln,find,echo,grep,cat,touch,tar,gzip,rm,tree,du,mlocate,updatedb,locate,tenet
+#     # add bogus user
+#     /filebrowser users add 1 bogus --database /config/filebrowser/filebrowser.db
+# fi
 
 data=/srv/data
 if [ ! \( -e "${data}" \) ]
@@ -45,6 +46,5 @@ fi
 #shellcheck disable=SC2086
 exec \
     /filebrowser \
-    --config /config/filebrowser/settings.json \
-    --database /config/filebrowser/filebrowser.db \
+    -c /config/filebrowser/config.yaml \
     "$@"
